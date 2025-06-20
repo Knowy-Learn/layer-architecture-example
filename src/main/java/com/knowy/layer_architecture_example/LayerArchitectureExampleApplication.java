77bd404e72d5cca49284226c47181b67b6032b41
saramforte <saraforte310@gmail.com>
2025-06-20 10:32:19 +0200

    First Commit

---
+++ src/main/java/com/knowy/layer_architecture_example/LayerArchitectureExampleApplication.java
@@ -0,0 +1,13 @@
+package com.knowy.layer_architecture_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayerArchitectureExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayerArchitectureExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowy/layer_architecture_example/LayerArchitectureExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowy.layer_architecture_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayerArchitectureExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
